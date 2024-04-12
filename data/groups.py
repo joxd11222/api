a5cfaaeea7141c96695e701aa5f357d728b5aba2
joxd11222 <157251766+joxd11222@users.noreply.github.com>
2024-04-12 20:38:15 +0200

    Update groups.py

--- data/groups.py
+++ data/groups.py
@@ -1,1 +1,11 @@
+groups = ["Group 1"]
 
+group1 = {
+  "users": ["John", "Jane", "Jack"],
+  "perms": ["read", "write", "delete"],
+  "date": "2023-01-01"
+}
+
+def get_users():
+  for x in group1["users"]:
+    print(f"User: {x}")
